7004e3a224032ef92b81cfe65b892e088a8380ee
Allan <allanfelipedk@gmail.com>
2023-07-08 23:40:13 -0300

    Types and Functions in C#

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# screen-sound
+# Screen-Sound
  c-sharp training at aura:Learn to program in C# with Object Orientation
+
+- Types and Functions in C#:
+    - Type variables of different types like string and int for storing different types of data;
+    - Developing the custom menu displaying our application's options and creating a way in which the user can choose the action they want.
+    - Conditional structures in C#, which make it possible for our program to execute different commands according to conditions set by user input.
--- Screen Sound/Program.cs
+++ Screen Sound/Program.cs
@@ -1,2 +1,53 @@
-﻿string message = "Welcome to Screen Sound!";
-Console.WriteLine(message);
+﻿string messageDeBoasVindas = "Bem vindo ao Screen Sound.";
+
+void ExibirMensagemDeBoasVindas()
+{
+    Console.WriteLine(@"
+░██████╗░█████╗░██████╗░███████╗███████╗███╗░░██╗  ░██████╗░█████╗░██╗░░░██╗██████╗░
+██╔════╝██╔══██╗██╔══██╗██╔════╝██╔════╝████╗░██║  ██╔════╝██╔══██╗██║░░░██║██╔══██╗
+╚█████╗░██║░░╚═╝██████╔╝█████╗░░█████╗░░██╔██╗██║  ╚█████╗░██║░░██║██║░░░██║██║░░██║
+░╚═══██╗██║░░██╗██╔══██╗██╔══╝░░██╔══╝░░██║╚████║  ░╚═══██╗██║░░██║██║░░░██║██║░░██║
+██████╔╝╚█████╔╝██║░░██║███████╗███████╗██║░╚███║  ██████╔╝╚█████╔╝╚██████╔╝██████╔╝
+╚═════╝░░╚════╝░╚═╝░░╚═╝╚══════╝╚══════╝╚═╝░░╚══╝  ╚═════╝░░╚════╝░░╚═════╝░╚═════╝░
+    
+");
+    Console.WriteLine(messageDeBoasVindas);
+}
+
+void ExibirOpcoesDoMenu()
+{
+    Console.WriteLine("\nDigite 1 para registrar uma banda.");
+    Console.WriteLine("Digite 2 para mostrar todas as bandas.");
+    Console.WriteLine("Digite 3 para avaliar uma banda.");
+    Console.WriteLine("Digite 4 para exibit a media de uma banda.");
+    Console.WriteLine("Digite -1 para sair do programa.");
+
+    Console.Write("\nEscolha uma opcao: ");
+    string input = Console.ReadLine();
+    int inputNumber = int.Parse(input);
+
+    switch (inputNumber)
+    {
+        case 1: 
+            Console.WriteLine("Voce escolheu a opcao: " + inputNumber);
+            break;
+        case 2:
+            Console.WriteLine("Voce escolheu a opcao: " + inputNumber);
+            break;
+        case 3:
+            Console.WriteLine("Voce escolheu a opcao: " + inputNumber);
+            break;
+        case 4:
+            Console.WriteLine("Voce escolheu a opcao: " + inputNumber);
+            break;
+        case -1:
+            Console.WriteLine("Voce saiu do programa!");
+            break;
+        default:
+            Console.WriteLine("[ERRO] opcao invalida!");
+            break;
+    }
+
+}
+ExibirMensagemDeBoasVindas();
+ExibirOpcoesDoMenu();
